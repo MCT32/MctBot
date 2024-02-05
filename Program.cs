46751eeb99f6648a60b19d718b196af29224bcb1
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-02-06 09:59:10 +1100

    Added postgres support and balance command

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿using DisCatSharp;
+﻿using System.Data;
+using DisCatSharp;
 using DisCatSharp.ApplicationCommands;
 using DisCatSharp.ApplicationCommands.Attributes;
 using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
 using DisCatSharp.Enums;
+using Npgsql;
 
 namespace MctBot
 {
@@ -17,6 +20,16 @@ namespace MctBot
 
         static async Task MainAsync()
         {
+            var postgresBuilder = new NpgsqlDataSourceBuilder();
+            postgresBuilder.ConnectionStringBuilder.Host = System.Environment.GetEnvironmentVariable("PGHOST");
+
+            var postgres = postgresBuilder.Build();
+
+            await postgres.CreateCommand(@"CREATE TABLE IF NOT EXISTS users (
+                id bigserial PRIMARY KEY,
+                balance integer NOT NULL DEFAULT 1000
+            );").ExecuteNonQueryAsync();
+
             var discord = new DiscordClient(new DiscordConfiguration()
             {
                 Token = System.Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
@@ -27,6 +40,7 @@ namespace MctBot
             var appCommands = discord.UseApplicationCommands();
 
             appCommands.RegisterGlobalCommands<PingCommand>();
+            appCommands.RegisterGlobalCommands<BalanceCommand>();
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
@@ -38,10 +52,35 @@ namespace MctBot
         [SlashCommand("ping", "Replies with pong!")]
         public async Task PingSlashCommand(InteractionContext ctx)
         {
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DisCatSharp.Entities.DiscordInteractionResponseBuilder()
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
             {
                 Content = "Pong!"
             });
+        }
+    }
+
+    public class BalanceCommand : ApplicationCommandsModule
+    {
+        [SlashCommand("balance", "Retrieve account balance.")]
+        public async Task BalanceSlashCommand(InteractionContext ctx, [Option("user", "User who's balance to get.")] DiscordUser user)
+        {
+            var postgresBuilder = new NpgsqlDataSourceBuilder();
+            postgresBuilder.ConnectionStringBuilder.Host = System.Environment.GetEnvironmentVariable("PGHOST");
+
+            var postgres = postgresBuilder.Build();
+
+            var balance = await postgres.CreateCommand($"SELECT balance FROM users WHERE id = {user.Id}").ExecuteScalarAsync();
+
+            if (balance == null)
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                {
+                    Content = $"User {user.GlobalName} is not registered yet!"
+                });
+            else
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                {
+                    Content = $"User {user.GlobalName} has ${balance} in the bank."
+                });
         }
     }
 }
