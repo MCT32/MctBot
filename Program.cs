a5d3570194d21982be54b5a01eef7ab9371fc312
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-02-06 08:12:48 +1100

    Basic bot setup

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using DisCatSharp;
+using DisCatSharp.Enums;
+
+namespace MctBot
+{
+    class MctBot
+    {
+        static void Main(string[] args)
+        {
+            DotNetEnv.Env.Load();
+
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = System.Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContent
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
